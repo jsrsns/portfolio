f835efa698130075755f9627ec276348dc3224b6
Joes Roosens <joesroosens@gmail.com>
2016-08-11 15:53:34 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,10 +3,10 @@ $(document).ready(function() {
   $('#header li').click(function() {
   	var selected = $(this).attr('class');
   	console.log(selected);
-  });
-  
-  if (selected === '.sorting') {
-    $('#content').append('<img src="' + "/img/Flag.svg" + '"/>');
+  	
+  	if (selected === '.sorting') {
+  	  $('#content').append('<img src="' + "/img/Flag.svg" + '"/>');
   }
+  });
   
 });
