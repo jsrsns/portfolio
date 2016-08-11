031790caffeb4884a8c37eeb16383ccdca28903f
Joes Roosens <joesroosens@gmail.com>
2016-08-11 17:05:35 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,21 +9,26 @@ $(document).ready(function() {
   	console.log(selected);
   	
   	if (selected === 'sorting') {
+  	  $('.info').hide();
   	  $('.pagination').show();
   	  $('#content').append('<img src="/img/Flag.svg"/>');
   	} else if (selected === 'architect') {
+  	  $('.info').hide();
   	  $('.pagination').show();
   	  $('#content').append('<img src="/img/Corner.svg"/>');
   	} else if (selected === 'isometric') {
+  	  $('.info').hide();
   	  $('.pagination').show();
   	  $('#content').append('<img src="/img/Stairs.svg"/>');
   	} else if (selected === 'flag') {
+  	  $('.info').hide();
   	  $('.pagination').show();
   	  $('#content').append('<img src="/img/Beam.svg"/>');
   	}
   });
   
   $('.header').click(function() {
+    $('.info').show();
     $('.pagination').hide();
     $('#content').empty();
   });
